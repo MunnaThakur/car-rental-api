c0349f5de93aad149e08e55514b8e446cc91af40
MunnaThakur <mkt0173@gmail.com>
2022-11-13 21:46:38 +0530

    add cors

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const allRoute = require("./routes/allRoutes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json({limit: "30mb", extended:true}));
 app.use(express.urlencoded({limit:"30mb", extended: true}));
+
+app.use(
+    cors({
+      origin: "*",
+      credentials: true,
+    })
+  );
+  
 
 //connect to mongodb
 mongoose.connect(process.env.MONGO_URI)
